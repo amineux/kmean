5813347492db280b5b3cccc1a38376b9fe0833df
amineux <amine.atarboulaich@gmail.com>
2020-06-04 09:59:39 +0200

    Update svm.py

--- svm.py
+++ svm.py
@@ -8,16 +8,9 @@
 # Pratique k-means Clustering
 
 
-
-# In[5]:
-
 get_ipython().magic(u'matplotlib inline')
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-# In[41]:
-
 
 from sklearn.datasets.samples_generator import make_blobs
 X, y = make_blobs(n_samples=300, centers=3,
@@ -26,24 +19,12 @@ print X.shape, y.shape
 
 plt.scatter(X[:, 0], X[:, 1], s=30);
 
-
-# In[42]:
-
-
 from sklearn.cluster import KMeans
 ktest = KMeans(n_clusters=3)
 ktest.fit(X) 
 y_pred = ktest.predict(X) 
 
-
 plt.scatter(X[:,0], X[:,1], c=y_pred)
-
-
-
-# In[54]:
-
-
-
 
 ktest = KMeans(n_clusters=4) 
 ktest.fit(X) 
@@ -51,24 +32,11 @@ y_pred = ktest.predict(X)
 
 plt.scatter(X[:,0], X[:,1], c=y_pred)
 
-
-
-
-# In[68]:
-
-
-
 ktest = KMeans(n_clusters=3, max_iter=100) 
 ktest.fit(X)
 y_pred = ktest.predict(X) 
 
-
 plt.scatter(X[:,0], X[:,1], c=y_pred)
-
-
-
-
-# In[72]:
 
 X, y = make_blobs(n_samples=300, centers=3,
                   random_state=0, cluster_std=0.85)
@@ -76,61 +44,29 @@ print X.shape, y.shape
 # Visualisation
 plt.scatter(X[:, 0], X[:, 1], s=30);
 
-
-# In[73]:
-
-
 ktest = KMeans(n_clusters=3, max_iter=100) 
 ktest.fit(X) 
 y_pred = ktest.predict(X)
-
-
 plt.scatter(X[:,0], X[:,1], c=y_pred)
-
-
-
-
-# In[122]:
 
 fig, ax=plt.subplots(1,2,figsize=(8,4))
 ax[0].scatter(X[:,0], X[:,1],c=y)
 ax[1].scatter(X[:,0], X[:,1],c=y_pred)
-
-
-
-# In[128]:
-
 from sklearn.datasets import load_digits
 digits = load_digits()
 print digits.keys()
-
-
 print digits.data.shape
 print digits.target
-
-
-
-
-# In[162]:
-
-
 X,y=digits.data, digits.target
 k_digits = KMeans(n_clusters=10)
 y_pred=k_digits.fit_predict(X)
 
 print k_digits.cluster_centers_.shape
 
-
-# In[155]:
-
-
-
 fig = plt.figure(figsize=(8,3))
 for i in range(10):
     ax = fig.add_subplot(2, 5, i+1, xticks=[], yticks=[])
     ax.imshow(np.reshape(k_digits.cluster_centers_[i],(8,8)), cmap=plt.cm.binary)
-
-
 
 from sklearn.decomposition import RandomizedPCA
 pca=RandomizedPCA(2).fit(X)
@@ -154,21 +90,14 @@ plt.imshow(img)
 print img.shape
 
 
-# In[193]:
-
-
 img_r = (img / 255.0).reshape(-1,3)
 print img_r.shape
 
-
-# In[195]:
 
 
 k_colors = KMeans(n_clusters=64).fit(img_r)
 y_pred=k_colors.predict(img_r)
 
-
-# In[224]:
 ster centers. We must have a total of 64 centroids, shape must be of the input 
 
 print k_colors.cluster_centers_.shape 
@@ -181,10 +110,6 @@ newimg=k_colors.cluster_centers_[k_colors.labels_]
 print newimg.shape 
 newimg=np.reshape(newimg, (img.shape))
 print newimg.shape 
-
-
-# In[233]:
-
 
 ax=fig.add_subplot(1,2,1,xticks=[],yticks=[],title='Original Image')
 ax.imshow(img)
